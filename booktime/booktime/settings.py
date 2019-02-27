4a0a5e1a8e690a50e1d34d9e7fc9a129feaa52b3
bahatiphill <gur79tub@gmail.com>
2019-02-27 16:29:36 +0200

    .gitignore secret_key and working Homepage

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g&tp389tsa@xiua)b^rdqsve(5@4=+o$xb-)mh+=44frsln&_^'
+SECRET_KEY = os.environ.get('SECRET_KET', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
