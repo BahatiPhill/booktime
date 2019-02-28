29254d051af30d44dd0343426f8dac02718228e2
bahatiphill <gur79tub@gmail.com>
2019-02-28 11:18:22 +0200

    basic contactus form and testing

--- booktime/booktime/settings.py
+++ booktime/booktime/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KET', None)
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,3 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = 'username'
+    EMAIL_HOST = 'smtp.domain.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = 'password'
+
+else:
+    EMAIL_BACKEND = (
+        'django.core.mail.backends.console.EmailBackend'
+    )
--- booktime/main/views.py
+++ booktime/main/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
+from main import forms
 # Create your views here.
+
+class ContactUsView(FormView):
+
+    template_name =  'contact_us.html'
+    form_class = forms.ContactForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        print('Am i working')
+        form.send_mail()
+        return super().form_valid(form)
+
+'''
+def contact_us(request):
+
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            form.send_mail()
+            return HttpResponseRedirect('/')
+
+    else:
+        form = forms.ContactForm()
+
+    return render(request, 'contact_us.html', {'form':form})
+'''
